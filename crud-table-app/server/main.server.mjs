22d319d2216b55e41207c04c482f826bed999c18
Shraddha <shraddha2510@gmail.com>
2025-01-04 16:55:07 +0530

    Auto-generated commit

--- crud-table-app/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-NY7IVXEB.mjs";import"./chunk-LWQXAAXU.mjs";import"./chunk-6CCMM3FU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-6FS7O4O7.mjs";import"./chunk-7YAQZ7NR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
